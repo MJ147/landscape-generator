2a5e4fdad9ed1808a9fcb88e6a2ced93762d2071
mj <jaczewski147@gmail.com>
2022-08-03 18:58:07 +0200

    add gui

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,9 +1,18 @@
+import { GUI } from 'dat.gui';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
-const gridSize = 1;
-const boardSize = 50;
-const heightFactor = 3;
+const boardSettings = {
+	fieldSize: 1,
+	boardSize: 30,
+	heightFactor: 3,
+};
+
+let lastHeightFactor = boardSettings.heightFactor;
+
+const cameraAutoRotate = {
+	rotateSpeed: 2,
+};
 
 const scene = new THREE.Scene();
 
@@ -21,42 +30,7 @@ const material = new THREE.MeshNormalMaterial({
 	vertexColors: true,
 });
 
-let heights: number[][] = [];
-
-for (let i = 0; i <= boardSize; i++) {
-	heights[i] = [];
-	for (let j = 0; j <= boardSize; j++) {
-		if (i === 0 || j === 0) {
-			heights[i][j] = Math.random() * gridSize * heightFactor;
-
-			continue;
-		}
-
-		// center board
-		const x = gridSize * (i - 0.5 * boardSize - 1);
-		const y = Math.random() * gridSize * heightFactor;
-		const z = gridSize * (j - 0.5 * boardSize - 1);
-
-		const vertices: number[] = [];
-		console.log(heights.map((v) => v.map((d) => Math.round(d)).toString()));
-
-		vertices.push(x, heights[i - 1][j - 1], z); // top left
-		vertices.push(x, heights[i - 1][j], z + gridSize); // bottom left
-		vertices.push(x + gridSize, heights[i][j - 1], z); // top right
-		vertices.push(x + gridSize, y, z + gridSize); // bottom right
-		vertices.push(x + gridSize, heights[i][j - 1], z); // top right
-		vertices.push(x, heights[i - 1][j], z + gridSize); // bottom left
-
-		const terrainGeometry = new THREE.BufferGeometry();
-		terrainGeometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
-		terrainGeometry.computeVertexNormals();
-
-		const terrain = new THREE.Mesh(terrainGeometry, material);
-		scene.add(terrain);
-
-		heights[i][j] = y;
-	}
-}
+createLandscape();
 
 window.addEventListener('resize', onWindowResize, false);
 
@@ -69,7 +43,14 @@ function onWindowResize() {
 
 function animate() {
 	requestAnimationFrame(animate);
-	scene.rotation.y += 0.002;
+	scene.rotation.y += cameraAutoRotate.rotateSpeed / 1000;
+
+	if (boardSettings.heightFactor !== lastHeightFactor) {
+		console.log(1);
+
+		lastHeightFactor = boardSettings.heightFactor;
+		createLandscape();
+	}
 
 	render();
 }
@@ -78,4 +59,57 @@ function render() {
 	renderer.render(scene, camera);
 }
 
+createGui();
 animate();
+
+function createGui() {
+	const gui = new GUI();
+	const cameraFolder = gui.addFolder('Camera');
+	cameraFolder.add(cameraAutoRotate, 'rotateSpeed', 0, 10, 1);
+	cameraFolder.open();
+
+	const boardFolder = gui.addFolder('Board');
+	boardFolder.add(boardSettings, 'heightFactor', 0, 10, 1);
+	boardFolder.open();
+}
+
+function createLandscape() {
+	scene.clear();
+
+	let heights: number[][] = [];
+
+	for (let i = 0; i <= boardSettings.boardSize; i++) {
+		heights[i] = [];
+
+		for (let j = 0; j <= boardSettings.boardSize; j++) {
+			if (i === 0 || j === 0) {
+				heights[i][j] = Math.random() * boardSettings.fieldSize * boardSettings.heightFactor;
+
+				continue;
+			}
+
+			// center board
+			const x = boardSettings.fieldSize * (i - 0.5 * boardSettings.boardSize - 1);
+			const y = Math.random() * boardSettings.fieldSize * boardSettings.heightFactor;
+			const z = boardSettings.fieldSize * (j - 0.5 * boardSettings.boardSize - 1);
+
+			const vertices: number[] = [];
+
+			vertices.push(x, heights[i - 1][j - 1], z); // top left
+			vertices.push(x, heights[i - 1][j], z + boardSettings.fieldSize); // bottom left
+			vertices.push(x + boardSettings.fieldSize, heights[i][j - 1], z); // top right
+			vertices.push(x + boardSettings.fieldSize, y, z + boardSettings.fieldSize); // bottom right
+			vertices.push(x + boardSettings.fieldSize, heights[i][j - 1], z); // top right
+			vertices.push(x, heights[i - 1][j], z + boardSettings.fieldSize); // bottom left
+
+			const terrainGeometry = new THREE.BufferGeometry();
+			terrainGeometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
+			terrainGeometry.computeVertexNormals();
+
+			const terrain = new THREE.Mesh(terrainGeometry, material);
+			scene.add(terrain);
+
+			heights[i][j] = y;
+		}
+	}
+}
