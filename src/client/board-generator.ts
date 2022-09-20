98acbf3158c544375a6113b11b81a1c42fa6bc99
mj <jaczewski147@gmail.com>
2022-09-20 19:22:05 +0200

    generate random seed

--- src/client/board-generator.ts
+++ src/client/board-generator.ts
@@ -1,4 +1,10 @@
 import { seededRandom } from 'three/src/math/MathUtils';
+import { BoardSettings } from './client';
+
+export interface Germination {
+	seed: number;
+	sprout: number;
+}
 
 export interface Board {
 	size: number;
@@ -9,32 +15,25 @@ export interface Board {
 // x, y, z
 export type Point = [number, number, number];
 
-export function generateSeed(): number {
-	const seed = Math.floor(seededRandom() * 10 ** 10);
+export function generateSeed(seed?: number): Germination {
+	seed = seed ?? Math.floor(Math.random() * 10 ** 10);
+	const sprout = Math.floor(seededRandom(seed) * 10 ** 10);
 
-	return seed;
+	return { seed, sprout };
 }
 
 export function getVertexYFromSeed(numberOfVertex: number, seed: number, max: number): number {
 	const seedArray = Array.from(seed.toString(), (n) => Number(n));
-	console.log(seedArray);
 
 	const first = seedArray[Math.floor(numberOfVertex / 1000)];
 	const second = seedArray[Math.floor((numberOfVertex / 100) % 10)];
 	const third = seedArray[Math.floor((numberOfVertex / 10) % 10)];
 	const four = seedArray[Math.floor(numberOfVertex % 10)];
-	console.log(first, second, third, four, max, (first + second + third + four) % max);
 
 	return (first + second + third + four) % (max + 1);
 }
 
-export function generateBoardModel(
-	seed: number,
-	boardSize: number,
-	maxHeight: number,
-	minHeight: number = 0,
-	verticesOffset: number = 1,
-): Board {
+export function generateBoardModel(seed: number, { boardSize, verticesOffset, maxHeight }: BoardSettings): Board {
 	let vertices: Point[][] = [];
 
 	for (let i = 0; i <= boardSize; i++) {
@@ -51,9 +50,9 @@ export function generateBoardModel(
 		}
 	}
 
-	console.log(vertices.map((r) => `[${r[0][0]}, ${r[0][2]}] (${r[0][1]}])`).toString());
-	console.log(vertices.map((r) => `[${r[1][0]}, ${r[1][2]}] (${r[1][1]}])`).toString());
-	console.log(vertices.map((r) => `[${r[2][0]}, ${r[2][2]}] (${r[2][1]}])`).toString());
+	// console.log(vertices.map((r) => `[${r[0][0]}, ${r[0][2]}] (${r[0][1]}])`).toString());
+	// console.log(vertices.map((r) => `[${r[1][0]}, ${r[1][2]}] (${r[1][1]}])`).toString());
+	// console.log(vertices.map((r) => `[${r[2][0]}, ${r[2][2]}] (${r[2][1]}])`).toString());
 
 	return {
 		size: boardSize,
--- src/client/client.ts
+++ src/client/client.ts
@@ -1,6 +1,7 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import { generateSeed, generateBoardModel, Point } from './board-generator';
+import { generateSeed, generateBoardModel, Point, Germination } from './board-generator';
+import { GUI } from 'dat.gui';
 
 export interface Grid {
 	topLeft: Point;
@@ -9,13 +10,32 @@ export interface Grid {
 	bottomLeft: Point;
 }
 
-const seed = generateSeed();
-const boardModel = generateBoardModel(seed, 50, 5);
+export interface BoardSettings {
+	boardSize: number;
+	maxHeight: number;
+	minHeight: number;
+	verticesOffset: number;
+}
+
+const germination: Germination = generateSeed();
+
+const boardSettings: BoardSettings = {
+	boardSize: 50,
+	maxHeight: 3,
+	minHeight: 0,
+	verticesOffset: 5,
+};
+
+let lastMaxHeight = boardSettings.maxHeight;
+
+const cameraSettings = {
+	rotateSpeed: 2,
+};
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 30;
-camera.position.y = 10;
+camera.position.y = 20;
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -27,91 +47,80 @@ const material = new THREE.MeshNormalMaterial({
 	vertexColors: true,
 });
 
-boardModel.vertices.forEach((verticesLine, i, verticesRow) => {
-	verticesLine.forEach((vertex, j) => {
-		if (i === 0 || j === 0) {
-			return;
-		}
+function generateBoard(): void {
+	scene.clear();
 
-		const grid: Grid = {
-			topLeft: verticesRow[i - 1][j - 1],
-			topRight: verticesLine[j - 1],
-			bottomRight: vertex,
-			bottomLeft: verticesRow[i - 1][j],
-		};
+	const boardModel = generateBoardModel(germination.sprout, boardSettings);
 
-		console.log(grid);
+	boardModel.vertices.forEach((verticesLine, i, verticesRow) => {
+		verticesLine.forEach((vertex, j) => {
+			if (i === 0 || j === 0) {
+				return;
+			}
 
-		const vertices: number[] = [];
+			const grid: Grid = {
+				topLeft: verticesRow[i - 1][j - 1],
+				topRight: verticesLine[j - 1],
+				bottomRight: vertex,
+				bottomLeft: verticesRow[i - 1][j],
+			};
 
-		vertices.push(...grid.topLeft);
-		vertices.push(...grid.bottomLeft);
-		vertices.push(...grid.topRight);
-		vertices.push(...grid.bottomRight);
-		vertices.push(...grid.topRight);
-		vertices.push(...grid.bottomLeft);
+			const vertices: number[] = [];
 
-		const terrainGeometry = new THREE.BufferGeometry();
-		terrainGeometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
-		terrainGeometry.computeVertexNormals();
+			vertices.push(...grid.topLeft);
+			vertices.push(...grid.bottomLeft);
+			vertices.push(...grid.topRight);
+			vertices.push(...grid.bottomRight);
+			vertices.push(...grid.topRight);
+			vertices.push(...grid.bottomLeft);
 
-		const terrain = new THREE.Mesh(terrainGeometry, material);
-		scene.add(terrain);
+			const terrainGeometry = new THREE.BufferGeometry();
+			terrainGeometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
+			terrainGeometry.computeVertexNormals();
+
+			const terrain = new THREE.Mesh(terrainGeometry, material);
+			scene.add(terrain);
+		});
 	});
-});
-
-// for (let i = 0; i <= boardSize; i++) {
-// 	heights[i] = [];
-// 	for (let j = 0; j <= boardSize; j++) {
-// 		if (i === 0 || j === 0) {
-// 			heights[i][j] = Math.random() * gridSize * heightFactor;
-
-// 			continue;
-// 		}
-
-// 		// center board
-// 		const x = gridSize * (i - 0.5 * boardSize - 1);
-// 		const y = Math.random() * gridSize * heightFactor;
-// 		const z = gridSize * (j - 0.5 * boardSize - 1);
-
-// 		const vertices: number[] = [];
-
-// 		vertices.push(x, heights[i - 1][j - 1], z); // top left
-// 		vertices.push(x, heights[i - 1][j], z + gridSize); // bottom left
-// 		vertices.push(x + gridSize, heights[i][j - 1], z); // top right
-// 		vertices.push(x + gridSize, y, z + gridSize); // bottom right
-// 		vertices.push(x + gridSize, heights[i][j - 1], z); // top right
-// 		vertices.push(x, heights[i - 1][j], z + gridSize); // bottom left
-
-// 		const terrainGeometry = new THREE.BufferGeometry();
-// 		terrainGeometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
-// 		terrainGeometry.computeVertexNormals();
-
-// 		const terrain = new THREE.Mesh(terrainGeometry, material);
-// 		scene.add(terrain);
-
-// 		heights[i][j] = y;
-// 	}
-// }
+}
 
 window.addEventListener('resize', onWindowResize, false);
+generateBoard();
 
-function onWindowResize() {
+function onWindowResize(): void {
 	camera.aspect = window.innerWidth / window.innerHeight;
 	camera.updateProjectionMatrix();
 	renderer.setSize(window.innerWidth, window.innerHeight);
 	render();
 }
 
-function animate() {
+function animate(): void {
 	requestAnimationFrame(animate);
-	// scene.rotation.y += 0.002;
+	scene.rotation.y += cameraSettings.rotateSpeed / 1000;
+
+	if (boardSettings.maxHeight !== lastMaxHeight) {
+		lastMaxHeight = boardSettings.maxHeight;
+		generateBoard();
+	}
 
 	render();
 }
 
-function render() {
+function render(): void {
 	renderer.render(scene, camera);
 }
 
+createGui();
 animate();
+
+function createGui(): void {
+	const gui = new GUI();
+	const cameraFolder = gui.addFolder('Camera');
+	cameraFolder.add(cameraSettings, 'rotateSpeed', 0, 10, 1);
+	cameraFolder.open();
+
+	const boardFolder = gui.addFolder('Board');
+	boardFolder.add(boardSettings, 'maxHeight', 0, 5, 1);
+	boardFolder.add(germination, 'seed');
+	boardFolder.open();
+}
